9d7d5a0ebf56bec86e27f543a3a39f9e70cde617
Nurbu <lamanurbu2@gmail.com>
2026-04-20 16:10:23 -0400

    Finished with printing the employees information from the csv.

--- src/main/java/com/pluralsight/EmployeeDirectory.java
+++ src/main/java/com/pluralsight/EmployeeDirectory.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class EmployeeDirectory {
+    public static void main(String[] args) {
+
+        try {
+            FileReader fileReader = new FileReader("employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] employeeData = line.split("\\|");
+                int id = Integer.parseInt(employeeData[0]);
+                String name = employeeData[1];
+                double hoursWorked = Double.parseDouble(employeeData[2]);
+                double payRate = Double.parseDouble(employeeData[3]);
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+
+                System.out.printf("Id: " + employee.getEmployeeId() + "\nName: " + employee.getName() + "\nGross Pay: " + employee.getGrossPay() + "\n");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
