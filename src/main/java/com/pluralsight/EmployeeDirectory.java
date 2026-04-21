5760eed72388e42723321b50cae9de2b31f025db
Nurbu <lamanurbu2@gmail.com>
2026-04-21 11:29:24 -0400

    Created the bufferedwriter.

--- src/main/java/com/pluralsight/EmployeeDirectory.java
+++ src/main/java/com/pluralsight/EmployeeDirectory.java
@@ -1,16 +1,25 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class EmployeeDirectory {
     // Reads employees.csv and prints their information
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the employee file to process: ");
+        String employeeFilePath = scanner.nextLine();
+        System.out.print("\nEnter the name of the payroll file to create: ");
+        String payrollFilePath = scanner.nextLine();
         try {
-            FileReader fileReader = new FileReader("employees.csv");
+            FileReader fileReader = new FileReader(employeeFilePath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(payrollFilePath));
             // Goes line by line of the csv file
             while ((line = bufferedReader.readLine()) != null) {
                 // Takes the line and splits it and then places the info into an array.
@@ -21,7 +30,8 @@ public class EmployeeDirectory {
                 double payRate = Double.parseDouble(employeeData[3]);
                 Employee employee = new Employee(id, name, hoursWorked, payRate);
 
-                System.out.printf("Id: " + employee.getEmployeeId() + "\nName: " + employee.getName() + "\nGross Pay: " + employee.getGrossPay() + "\n");
+                
+                // System.out.printf("Id: " + employee.getEmployeeId() + "\nName: " + employee.getName() + "\nGross Pay: " + employee.getGrossPay() + "\n");
             }
 
         } catch (Exception e) {
