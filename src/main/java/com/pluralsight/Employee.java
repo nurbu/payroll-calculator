6ecdd26fe137432a95830b0bc7b7d1da86fbecc4
Nurbu <lamanurbu2@gmail.com>
2026-04-21 10:02:06 -0400

    got rid of the setter EmployeeId method.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -17,10 +17,6 @@ public class Employee {
         return employeeId;
     }
 
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
     public String getName() {
         return name;
     }
